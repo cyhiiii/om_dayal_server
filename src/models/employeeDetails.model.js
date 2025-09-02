609be3e8eb56686cfea495656130f1d6dee62d0f
aditya-9613 <155987958+aditya-9613@users.noreply.github.com>
2025-09-02 18:46:19 +0000

    feat: Add EmployeeDetails model with schema definition

---
+++ src/models/employeeDetails.model.js
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+
+const EmployeeDetailsSchema = new Schema({
+    employeeUsername: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    profileImage:{
+        type: String,
+    },
+    employeeCode: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    mobile:{
+        type: String,
+        required: true,
+    },
+    alternateNumber:{
+        type: String,
+    },
+    fatherName:{
+        type: String,
+    },
+    address:{
+        type: String,
+    },
+    document_number:{
+        type: String,
+        required
+    },
+    adharCardFront:{
+        type: String,
+    },
+    adharCardBack:{
+        type: String,
+    },
+    highestQualification:{
+        type: String,
+    }
+},
+{
+    timestamps: true
+})
+
+export const EmployeeDetails = mongoose.model('EmployeeDetails', EmployeeDetailsSchema)
