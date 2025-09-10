812c28d9ac74fbc94882268504f9e0cf752d7fd2
aditya-9613 <155987958+aditya-9613@users.noreply.github.com>
2025-09-10 07:25:46 +0000

    fix: Update CORS configuration to explicitly allow all origins

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin:"*",
+}))
 
 app.use(express.json({limit: "1mb"}))
 app.use(express.urlencoded({extended: true, limit: "1mb"}))
